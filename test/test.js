72323f33b86ee038ff0812e1770082abd8ba77d2
Seo_Hyun <04seohyun@gmail.com>
2020-10-25 18:31:23 +0900

    Fix for #33

--- test/test.js
+++ test/test.js
@@ -1,24 +1,24 @@
-const {BrowserWindow} = require('../index.js');
-const {app} = require('electron');
-const os = require("os");
+const {BrowserWindow} = require('../index.js')
+const {app} = require('electron')
+const os = require("os")
 
-let win;
+let win
 
 function isWindows10() {
-    if (process.platform !== 'win32') return false;
-    return os.release().split('.')[0] === '10';
+    if (process.platform !== 'win32') return false
+    return os.release().split('.')[0] === '10'
 }
 
 function createWindow() {
-    let vibrancyOp;
+    let vibrancyOp
     if (isWindows10()) vibrancyOp = {
         theme: '#661237cc',
         effect: 'acrylic',
         useCustomWindowRefreshMethod: true,
         disableOnBlur: true,
         debug: true
-    };
-    else vibrancyOp = 'dark';
+    }
+    else vibrancyOp = 'dark'
     win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -28,21 +28,23 @@ function createWindow() {
             enableRemoteModule: true
         },
         vibrancy: vibrancyOp
-    });
-    win.loadURL(`file://${__dirname}/test.html`);
-    //win.webContents.openDevTools({mode: "detach"});
+    })
+    win.loadURL(`file://${__dirname}/test.html`)
+    //win.webContents.openDevTools({mode: "detach"})
+
+    win.webContents.openDevTools()
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit();
+        app.quit()
     }
-});
+})
 
 app.on('activate', () => {
     if (win === null) {
-        createWindow();
+        createWindow()
     }
-});
+})
