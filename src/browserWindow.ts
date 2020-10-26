07a50b567c637442f9c06f76c92858a3415b1b95
Elias Skogevall <elias.skogevall@gmail.com>
2020-10-26 10:36:52 +0100

    Transform JavaScript to TypeScript

--- src/browserWindow.ts
+++ src/browserWindow.ts
@@ -2,7 +2,7 @@ import * as electron from 'electron'
 import { VibrancyOptions } from 'electron-acrylic-window';
 import debug from './debug';
 import { isRS4OrGreater, isWindows10 } from './os';
-import { getConfigFromOptions, rgbToHex, Vibrancy, VibrancyConfig, WindowConfig, windowConfigs, _setVibrancy } from './vibrancy';
+import { getConfigFromOptions, rgbToHex, Vibrancy, VibrancyConfig, WindowConfig, _setVibrancy } from './vibrancy';
 
 /**
  * Allow modifying default BrowserWindowConstructorOptions
@@ -14,145 +14,110 @@ type Modify<T, R> = Omit<T, keyof R> & R;
  * The new options of the BrowserWindow with the VibrancyOptions.
  */
 export type AcrylicBrowserWindowConstructorOptions = Modify<electron.BrowserWindowConstructorOptions, {
-
-    /**
-     * The vibrancy settings for the window. Can be
-     * a VibrancyTheme or the VibrancyOptions object.
-     */
-    vibrancy?: Vibrancy
+	/**
+	 * The vibrancy settings for the window. Can be
+	 * a VibrancyTheme or the VibrancyOptions object.
+	 */
+	vibrancy?: Vibrancy
 }>;
 
 export class BrowserWindow extends electron.BrowserWindow {
-    // https://github.com/microsoft/TypeScript/issues/30071
-    // @ts-ignore
-    setVibrancy(options?: Vibrancy) {
-        if (options) {
-            if (windowConfigs[this.id]) {
-                windowConfigs[this.id].vibrnacyConfig = getConfigFromOptions(options);
-            } else {
-                windowConfigs[this.id] = {
-                    vibrancyActivated: false,
-                    vibrnacyConfig: getConfigFromOptions(options)
-                }
-            }
-            
-            _setVibrancy(this, windowConfigs[this.id].vibrnacyConfig);
-        } else {
-            // If disabling vibrancy, turn off then save
-            _setVibrancy(this)
+	/**
+	 * Set the vibrancy for the specified window.
+	 *
+	 * @param options
+	 */
+	// https://github.com/microsoft/TypeScript/issues/30071
+	// @ts-ignore
+	setVibrancy(options?: Vibrancy) {
+		if (options) {
+			this.#winconfig.vibrnacyConfig = getConfigFromOptions(options);
+			_setVibrancy(this, this.#winconfig.vibrnacyConfig);
+		} else {
+			// If disabling vibrancy, turn off then save
+			_setVibrancy(this)
+			this.#winconfig.vibrnacyConfig = getConfigFromOptions(undefined);
+		}
+	}
 
-            if (windowConfigs[this.id]) {
-                windowConfigs[this.id].vibrnacyConfig = getConfigFromOptions(undefined);
-            } else {
-                windowConfigs[this.id] = {
-                    vibrancyActivated: false,
-                    vibrnacyConfig: getConfigFromOptions(undefined)
-                }
-            }
-        }
-    }
+	#vibconfig = getConfigFromOptions(this.options?.vibrancy)
 
-    constructor(options?: AcrylicBrowserWindowConstructorOptions) {
-        super(Object.assign(options, { vibrancy: undefined }))
+	#winconfig: WindowConfig = {
+		targetOpacity: 0,
+		vibrancyActivated: false,
+		vibrnacyConfig: this.#vibconfig,
+		opacity: 0,
+		currentOpacity: 0,
+		opacityInterval: undefined
+	}
 
-        let oShow = options?.show ?? true;
+	constructor(private options?: AcrylicBrowserWindowConstructorOptions) {
+		super(Object.assign(options, { vibrancy: undefined }))
 
-        let config = getConfigFromOptions(options?.vibrancy);
+		let oShow = options?.show ?? true;
 
-        if (isWindows10 && config) {
-            if (config.colors.base)
-                this.setBackgroundColor(rgbToHex(config.colors.base))
-            this.hide()
-        }
+		let config = getConfigFromOptions(options?.vibrancy);
 
-        const id = this.id
-        const winConf: WindowConfig = new class {
-            private _vibrnacyConfig = config
-            public get vibrnacyConfig(): VibrancyConfig {
-                return this._vibrnacyConfig;
-            }
-            public set vibrnacyConfig(v: VibrancyConfig) {
-                this._vibrnacyConfig = windowConfigs[id].vibrnacyConfig = v;
-            }
+		if (isWindows10 && config) {
+			if (config.colors.base)
+				this.setBackgroundColor(rgbToHex(config.colors.base))
+			this.hide()
+		}
 
+		if (isWindows10 && config && config.useCustomWindowRefreshMethod) { }
 
-            private _vibrancyActivated = false
-            public get vibrancyActivated(): boolean {
-                return this._vibrancyActivated;
-            }
-            public set vibrancyActivated(v: boolean) {
-                this._vibrancyActivated = windowConfigs[id].vibrancyActivated = v;
-            }
+		if (config && config.disableOnBlur) {
+			this.#winconfig.opacity = 0
 
-            constructor() {
-                windowConfigs[id] = {
-                    vibrancyActivated: this.vibrancyActivated,
-                    vibrnacyConfig: this.vibrnacyConfig
-                }
-            }
-        }
+			this.on('blur', () => {
+				if (isWindows10 && this.#winconfig) {
+					this.#winconfig.targetOpacity = 255
+					if (!this.#winconfig.opacityInterval)
+						this.#winconfig.opacityInterval = setInterval(() => {
+							try {
+								let colorDiff = (255 - this.#winconfig.vibrnacyConfig.colors.a) / 3.5
+								if (Math.abs(this.#winconfig.currentOpacity - this.#winconfig.targetOpacity) < colorDiff) {
+									this.#winconfig.currentOpacity = this.#winconfig.targetOpacity
+									clearInterval(this.#winconfig.opacityInterval)
+									this.#winconfig.opacityInterval = 0
+								} else if (this.#winconfig.currentOpacity > this.#winconfig.targetOpacity) this.#winconfig.currentOpacity -= colorDiff
+								else this.#winconfig.currentOpacity += colorDiff
+								_setVibrancy(this, this.#winconfig.vibrnacyConfig)
+							} catch (e) {
 
-        if (isWindows10 && config && config.useCustomWindowRefreshMethod) { }
+							}
+						}, 1000 / 30)
+				}
+			})
 
-        if (config && config.disableOnBlur) {
-            win._vibrancyOp.opacity = 0
+			this.on('focus', () => {
+				if (isWindows10 && this.#winconfig) {
+					this.#winconfig.targetOpacity = this.#winconfig.vibrnacyConfig.colors.a
+					if (!this.#winconfig.opacityInterval)
+						this.#winconfig.opacityInterval = setInterval(() => {
+							try {
+								let colorDiff = (255 - this.#winconfig.vibrnacyConfig.colors.a) / 3.5
+								if (Math.abs(this.#winconfig.currentOpacity - this.#winconfig.targetOpacity) < colorDiff) {
+									this.#winconfig.currentOpacity = this.#winconfig.targetOpacity
+									clearInterval(this.#winconfig.opacityInterval)
+									this.#winconfig.opacityInterval = undefined
+								} else if (this.#winconfig.currentOpacity > this.#winconfig.targetOpacity) this.#winconfig.currentOpacity -= colorDiff
+								else this.#winconfig.currentOpacity += colorDiff
+								_setVibrancy(this, this.#winconfig.vibrnacyConfig)
+							} catch (e) {
 
-            win.on('blur', () => {
-                if (isWindows10() && win._vibrancyOp) {
-                    win._vibrancyOp.targetOpacity = 255
-                    if (!win._vibrancyOp.opacityInterval)
-                        win._vibrancyOp.opacityInterval = setInterval(() => {
-                            try {
-                                let colorDiff = (255 - win._vibrancyOp.colors.a) / 3.5
-                                if (Math.abs(win._vibrancyOp.currentOpacity - win._vibrancyOp.targetOpacity) < colorDiff) {
-                                    win._vibrancyOp.currentOpacity = win._vibrancyOp.targetOpacity
-                                    clearInterval(win._vibrancyOp.opacityInterval)
-                                    win._vibrancyOp.opacityInterval = 0
-                                } else if (win._vibrancyOp.currentOpacity > win._vibrancyOp.targetOpacity) win._vibrancyOp.currentOpacity -= colorDiff
-                                else win._vibrancyOp.currentOpacity += colorDiff
-                                _setVibrancy(win, win._vibrancyOp)
-                            } catch (e) {
+							}
+						}, 1000 / 30)
+				}
+			})
+		}
 
-                            }
-                        }, 1000 / 30)
-                }
-            })
-
-            win.on('focus', () => {
-                if (isWindows10() && win._vibrancyOp) {
-                    win._vibrancyOp.targetOpacity = win._vibrancyOp.colors.a
-                    if (!win._vibrancyOp.opacityInterval)
-                        win._vibrancyOp.opacityInterval = setInterval(() => {
-                            try {
-                                let colorDiff = (255 - win._vibrancyOp.colors.a) / 3.5
-                                if (Math.abs(win._vibrancyOp.currentOpacity - win._vibrancyOp.targetOpacity) < colorDiff) {
-                                    win._vibrancyOp.currentOpacity = win._vibrancyOp.targetOpacity
-                                    clearInterval(win._vibrancyOp.opacityInterval)
-                                    win._vibrancyOp.opacityInterval = 0
-                                } else if (win._vibrancyOp.currentOpacity > win._vibrancyOp.targetOpacity) win._vibrancyOp.currentOpacity -= colorDiff
-                                else win._vibrancyOp.currentOpacity += colorDiff
-                                _setVibrancy(win, win._vibrancyOp)
-                            } catch (e) {
-
-                            }
-                        }, 1000 / 30)
-                }
-            })
-        }
-
-        if (isWindows10() && options.hasOwnProperty('vibrancy')) win.once('ready-to-show', () => {
-            setTimeout(() => {
-                if (oShow) win.show();
-                win.setVibrancy(win._vibrancyOp);
-            }, 100);
-        });
-
-        return win;
-    }
-    static _bindAndReplace(object, method) {
-        const boundFunction = method.bind(object);
-        Object.defineProperty(object, method.name, {
-            get: () => boundFunction
-        });
-    }
+		if (isWindows10 && options && Object.prototype.hasOwnProperty.call(options, 'vibrancy'))
+			this.once('ready-to-show', () => {
+				setTimeout(() => {
+					if (oShow) this.show();
+					this.setVibrancy(options?.vibrancy);
+				}, 100);
+			});
+	}
 }
--- src/vibrancy.ts
+++ src/vibrancy.ts
@@ -1,15 +1,16 @@
 import * as bindings from './bindings'
 import { BrowserWindow } from './browserWindow'
 import debug from './debug'
-import { isWindows10 } from './os'
+import { isRS4OrGreater, isWindows10 } from './os'
+import * as electron from 'electron'
 
 function getHwnd(win: BrowserWindow) {
-    if (!win) throw new TypeError('WINDOW_NOT_GIVEN')
-    try {
-        return win.getNativeWindowHandle().readInt32LE(0)
-    } catch (e) {
-        throw new TypeError('NOT_VALID_WINDOW')
-    }
+	if (!win) throw new TypeError('WINDOW_NOT_GIVEN')
+	try {
+		return win.getNativeWindowHandle().readInt32LE(0)
+	} catch (e) {
+		throw new TypeError('NOT_VALID_WINDOW')
+	}
 }
 const supportedType = ['light', 'dark', 'appearance-based']
 
@@ -17,98 +18,100 @@ const _lightThemeColor: [221, 221, 221, 136] = [221, 221, 221, 136]
 const _darkThemeColor: [34, 34, 34, 136] = [34, 34, 34, 136]
 
 export interface WindowConfig {
-    vibrancyActivated: boolean
-    vibrnacyConfig: VibrancyConfig
+	vibrancyActivated: boolean
+	vibrnacyConfig: VibrancyConfig
+	targetOpacity: number
+	opacity: number
+	opacityInterval: any
+	currentOpacity: number
 }
 
 export interface RGBA extends RGB {
-    a: number
+	a: number
 }
 
 export interface RGB {
-    r: number
-    g: number
-    b: number
+	r: number
+	g: number
+	b: number
 }
 
 export interface VibrancyConfig {
-    colors: RGBA & { base?: RGBA }
-    effect: 0 | 1;
-    useCustomWindowRefreshMethod?: boolean;
-    maximumRefreshRate?: number;
-    disableOnBlur?: boolean;
-    currentOpacity: number
+	colors: RGBA & { base?: RGBA }
+	effect: 0 | 1;
+	useCustomWindowRefreshMethod?: boolean;
+	maximumRefreshRate?: number;
+	disableOnBlur?: boolean;
+	currentOpacity: number
 }
 
-export const windowConfigs: Record<number, WindowConfig> = {}
-
 export function hexTo255(hex: string) {
-    const result = /^#?([a-f\d]{2})$/i.exec(hex);
-    return result ? parseInt(result[1], 16) : undefined;
+	const result = /^#?([a-f\d]{2})$/i.exec(hex);
+	return result ? parseInt(result[1], 16) : undefined;
 }
 
 export function _8bitToHex(c: number) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+	var hex = c.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
 }
 
 export function rgbToHex(rgb: RGB | RGBA) {
-    return "#" + _8bitToHex(rgb.r) + _8bitToHex(rgb.g) + _8bitToHex(rgb.b);
+	return "#" + _8bitToHex(rgb.r) + _8bitToHex(rgb.g) + _8bitToHex(rgb.b);
 }
 
 function getColorsFromTheme(theme: VibrancyOptions['theme']): VibrancyConfig['colors'] {
-    const unkownTheme = typeof theme === 'string' && supportedType.indexOf(theme) === -1
+	const unkownTheme = typeof theme === 'string' && supportedType.indexOf(theme) === -1
 
-    const dark = {
-        r: _darkThemeColor[0],
-        g: _darkThemeColor[1],
-        b: _darkThemeColor[2],
-        a: _darkThemeColor[3]
-    };
+	const dark = {
+		r: _darkThemeColor[0],
+		g: _darkThemeColor[1],
+		b: _darkThemeColor[2],
+		a: _darkThemeColor[3]
+	};
 
-    const light = {
-        r: _lightThemeColor[0],
-        g: _lightThemeColor[1],
-        b: _lightThemeColor[2],
-        a: _lightThemeColor[3]
-    };
+	const light = {
+		r: _lightThemeColor[0],
+		g: _lightThemeColor[1],
+		b: _lightThemeColor[2],
+		a: _lightThemeColor[3]
+	};
 
-    if (unkownTheme || theme === 'appearance-based') {
-        if (electron.nativeTheme.shouldUseDarkColors)
-            // dark
-            return dark
-        else
-            return light
-    }
+	if (unkownTheme || theme === 'appearance-based') {
+		if (electron.nativeTheme.shouldUseDarkColors)
+			// dark
+			return dark
+		else
+			return light
+	}
 
-    if (theme === 'light')
-        return light
+	if (theme === 'light')
+		return light
 
-    if (theme === 'dark')
-        return dark
+	if (theme === 'dark')
+		return dark
 
-    if (typeof theme === 'object') {
-        if ('hex' in theme) {
-            const r = hexTo255(theme.hex.slice(0, 2));
-            const g = hexTo255(theme.hex.slice(2, 4));
-            const b = hexTo255(theme.hex.slice(4, 6));
-            const a = hexTo255(theme.hex.slice(6, 8));
+	if (typeof theme === 'object') {
+		if ('hex' in theme) {
+			const r = hexTo255(theme.hex.slice(0, 2));
+			const g = hexTo255(theme.hex.slice(2, 4));
+			const b = hexTo255(theme.hex.slice(4, 6));
+			const a = hexTo255(theme.hex.slice(6, 8));
 
-            if (!(r && g && b && a))
-                return light
+			if (!(r && g && b && a))
+				return light
 
-            return { r, g, b, a }
-        } else if ('rgba' in theme) {
-            return {
-                r: theme.rgba[0],
-                g: theme.rgba[1],
-                b: theme.rgba[2],
-                a: theme.rgba[3]
-            }
-        }
-    }
+			return { r, g, b, a }
+		} else if ('rgba' in theme) {
+			return {
+				r: theme.rgba[0],
+				g: theme.rgba[1],
+				b: theme.rgba[2],
+				a: theme.rgba[3]
+			}
+		}
+	}
 
-    return light
+	return light
 }
 
 /**
@@ -127,110 +130,127 @@ export type VibrancyEffect = 'acrylic' | 'blur';
  * The vibrancy object
  */
 export interface VibrancyOptions {
-    /**
-     * The theme to use.
-     */
-    theme?: VibrancyTheme;
+	/**
+	 * The theme to use.
+	 */
+	theme?: VibrancyTheme;
 
-    /**
-     * The effect to use.
-     */
-    effect?: VibrancyEffect;
+	/**
+	 * The effect to use.
+	 */
+	effect?: VibrancyEffect;
 
-    /**
-     * If enabled, we use a custom window resize/move
-     * handler for performance.
-     */
-    useCustomWindowRefreshMethod?: boolean;
+	/**
+	 * If enabled, we use a custom window resize/move
+	 * handler for performance.
+	 */
+	useCustomWindowRefreshMethod?: boolean;
 
-    /**
-     * Maximum value to refresh application screen
-     * in seconds.
-     */
-    maximumRefreshRate?: number;
+	/**
+	 * Maximum value to refresh application screen
+	 * in seconds.
+	 */
+	maximumRefreshRate?: number;
 
-    /**
-     * If true, acrylic effect will be disabled whe
-     * window lost focus.
-     */
-    disableOnBlur?: boolean;
+	/**
+	 * If true, acrylic effect will be disabled whe
+	 * window lost focus.
+	 */
+	disableOnBlur?: boolean;
 }
 
 export type Vibrancy = VibrancyTheme | VibrancyOptions
 
 export function getConfigFromOptions(vibrancyOptions: Vibrancy | undefined): VibrancyConfig {
-    const defaultSettings: VibrancyOptions = {
-        theme: undefined,
-        effect: 'acrylic',
-        useCustomWindowRefreshMethod: true,
-        maximumRefreshRate: 60,
-        disableOnBlur: true
-    }
+	const defaultSettings: VibrancyOptions = {
+		theme: undefined,
+		effect: 'acrylic',
+		useCustomWindowRefreshMethod: true,
+		maximumRefreshRate: 60,
+		disableOnBlur: true
+	}
 
-    const options = Object.assign(defaultSettings, typeof vibrancyOptions === "object" ? vibrancyOptions : { theme: vibrancyOptions })
+	const options = Object.assign(defaultSettings, typeof vibrancyOptions === "object" ? vibrancyOptions : { theme: vibrancyOptions })
 
-    // Merge provided settings into defaults
-    let config: VibrancyConfig = {
-        disableOnBlur: options.disableOnBlur,
-        effect: 0,
-        maximumRefreshRate: options.maximumRefreshRate,
-        useCustomWindowRefreshMethod: options.useCustomWindowRefreshMethod,
-        colors: getColorsFromTheme(options.theme),
-        currentOpacity: 0
-    }
+	// Merge provided settings into defaults
+	let config: VibrancyConfig = {
+		disableOnBlur: options.disableOnBlur,
+		effect: 0,
+		maximumRefreshRate: options.maximumRefreshRate,
+		useCustomWindowRefreshMethod: options.useCustomWindowRefreshMethod,
+		colors: getColorsFromTheme(options.theme),
+		currentOpacity: 0
+	}
 
-    // Set blur type
-    if (options.effect === 'acrylic' && isRS4OrGreater)
-        config.effect = 1;
+	// Set blur type
+	if (options.effect === 'acrylic' && isRS4OrGreater)
+		config.effect = 1;
 
-    // Debug output
-    if (debug)
-        console.log(config)
+	// Debug output
+	if (debug)
+		console.log(config)
 
-    return config;
+	return config;
 }
 
 export function _setVibrancy(win: BrowserWindow, config?: VibrancyConfig) {
-    const winConfig = windowConfigs[win.id]
+	const winConfig = windowConfigs[win.id]
 
-    if (config && config.colors) {
-        if (debug) console.log("Vibrancy On", config)
-        bindings.setVibrancy(getHwnd(win), config.effect, config.colors.r, config.colors.g, config.colors.b, winConfig.vibrnacyConfig.currentOpacity);
-        winConfig.vibrancyActivated = true;
-        setTimeout(() => {
-            try {
-                if (winConfig.vibrancyActivated) win.setBackgroundColor('#00000000');
-            } catch (e) {
+	if (config && config.colors) {
+		if (debug) console.log("Vibrancy On", config)
+		bindings.setVibrancy(getHwnd(win), config.effect, config.colors.r, config.colors.g, config.colors.b, winConfig.vibrnacyConfig.currentOpacity);
+		winConfig.vibrancyActivated = true;
+		setTimeout(() => {
+			try {
+				if (winConfig.vibrancyActivated) win.setBackgroundColor('#00000000');
+			} catch (e) {
 
-            }
-        }, 50);
-    } else {
-        if (debug) console.log("Vibrancy Off", config, winConfig.vibrnacyConfig)
-        winConfig.vibrancyActivated = false;
-        if (winConfig.vibrnacyConfig) {
-            win.setBackgroundColor((winConfig.vibrnacyConfig && winConfig.vibrnacyConfig.colors ? "#FE" + winConfig.vibrnacyConfig.colors.r + winConfig.vibrnacyConfig.colors.g + winConfig.vibrnacyConfig.colors.b : "#000000"));
-        }
-        setTimeout(() => {
-            try {
-                if (!winConfig.vibrancyActivated) bindings.disableVibrancy(getHwnd(win));
-            } catch (e) {
+			}
+		}, 50);
+	} else {
+		if (debug) console.log("Vibrancy Off", config, winConfig.vibrnacyConfig)
+		winConfig.vibrancyActivated = false;
+		if (winConfig.vibrnacyConfig) {
+			win.setBackgroundColor((winConfig.vibrnacyConfig && winConfig.vibrnacyConfig.colors ? "#FE" + winConfig.vibrnacyConfig.colors.r + winConfig.vibrnacyConfig.colors.g + winConfig.vibrnacyConfig.colors.b : "#000000"));
+		}
+		setTimeout(() => {
+			try {
+				if (!winConfig.vibrancyActivated) bindings.disableVibrancy(getHwnd(win));
+			} catch (e) {
 
-            }
-         }, 10);
-    }
+			}
+		}, 10);
+	}
 }
 
+/**
+ * Set the vibrancy for the specified window.
+ *
+ * @param options
+ */
 export function setVibrancy(win: BrowserWindow, vibrancy: Vibrancy = 'appearance-based') {
-    // If disabling vibrancy, turn off then save
-    if (!vibrancy) {
-        _setVibrancy(win);
-        win._vibrancyOp = getConfigFromOptions(vibrancy);
-    } else {
-        win._vibrancyOp = getConfigFromOptions(vibrancy);
-        if (!isWindows10) win.setVibrancy(win._vibrancyOp);
-        else {
-            if (!vibrancy) _setVibrancy(win);
-            else _setVibrancy(win, win._vibrancyOp);
-        }
-    }
+	if (vibrancy) {
+		if (windowConfigs[win.id]) {
+			windowConfigs[win.id].vibrnacyConfig = getConfigFromOptions(vibrancy);
+		} else {
+			windowConfigs[win.id] = {
+				vibrancyActivated: false,
+				vibrnacyConfig: getConfigFromOptions(vibrancy)
+			}
+		}
+
+		_setVibrancy(win, windowConfigs[win.id].vibrnacyConfig);
+	} else {
+		// If disabling vibrancy, turn off then save
+		_setVibrancy(win)
+
+		if (windowConfigs[win.id]) {
+			windowConfigs[win.id].vibrnacyConfig = getConfigFromOptions(undefined);
+		} else {
+			windowConfigs[win.id] = {
+				vibrancyActivated: false,
+				vibrnacyConfig: getConfigFromOptions(undefined)
+			}
+		}
+	}
 }
