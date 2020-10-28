f9fc5226705f98b8a991f00a6864a5e83e1e9403
Elias Skogevall <elias.skogevall@gmail.com>
2020-10-28 12:03:35 +0100

    Remove toggleDebugging export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { BrowserWindow, AcrylicBrowserWindowConstructorOptions } from './browserWindow';
 export { setVibrancy, RGB, RGBA, Vibrancy, VibrancyEffect, VibrancyOptions, VibrancyTheme } from './vibrancy'
-export { toggleDebugging as debug } from './debug'
