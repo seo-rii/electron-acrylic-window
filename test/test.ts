3c5ac92b87125abe60c713369106755e9ca143a7
Seo-Rii <StudioSeoRii@gmail.com>
2021-03-17 14:25:41 +0900

    Change test script to ts

---
+++ test/test.ts
@@ -0,0 +1,54 @@
+import {BrowserWindow, Vibrancy} from '../build_ts'
+import {toggleDebugging as debug} from '../build_ts/debug'
+import {app} from 'electron'
+import * as os from 'os'
+import * as path from 'path'
+
+let win: BrowserWindow;
+
+const isWindows10 = process.platform === 'win32' && os.release().split('.')[0] === '10'
+
+function createWindow() {
+	// activate debugging
+	debug(true)
+
+	let vibrancyOp: Vibrancy;
+
+	if (isWindows10)
+		vibrancyOp = {
+			theme: '#661237cc',
+			effect: 'acrylic',
+			useCustomWindowRefreshMethod: true,
+			disableOnBlur: true,
+			debug: false
+		};
+	else
+		vibrancyOp = 'dark';
+
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		frame: false,
+		webPreferences: {
+			nodeIntegration: true,
+			enableRemoteModule: true
+		},
+		vibrancy: vibrancyOp
+	});
+
+	win.loadFile(path.join(__dirname, 'test.html'));
+
+	win.show()
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin')
+		app.quit()
+});
+
+app.on('activate', () => {
+	if (win === null)
+		createWindow();
+});
